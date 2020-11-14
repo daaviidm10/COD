ff3ab9fa0e019d8db4f10e0ebb2351e41f978957
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2020-11-14 17:10:31 +0100

    Nuevo

---
+++ src/COD/PrimerProyecto.java
@@ -0,0 +1,14 @@
+package COD;
+
+
+public class Primerproyecto {
+    /**
+     * Detalles del método
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Primer proyecto subido");
+    }
+
+
+}
