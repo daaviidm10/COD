8bbcca7ff768f0492329920e6b771ac0d8f03982
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2020-11-14 17:15:23 +0100

    Nuevo

---
+++ src/COD/SegundoProyectoFormateo.java
@@ -0,0 +1,26 @@
+package COD;
+
+
+/**
+ * Datos
+ * @author David Mariño Alonso
+ * @version 0.0.1
+ *
+ */
+public class SegundoProyectoFormateo {
+    /**
+     * Detalles del método
+     *
+     */
+    public static void main(String[] args) {
+    }
+    /**
+     *
+     * @param radio corresponde ao valor do radio.
+     * @return corresponde á resolución da operación.
+     */
+    public float calcular(double radio){
+        return (float) (radio*2*3.14);
+    }
+
+}
