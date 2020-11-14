d1921a2088ca5294855cec6596c0887ed07faf18
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2020-11-14 17:04:43 +0100

    sdasd

---
+++ src/com/cod/PrimerProyecto.java
@@ -0,0 +1,20 @@
+package com.programacion.boletin1;
+
+
+public class Primerproyecto {
+    /**
+     * Detalles del método
+     *
+     */
+    public static void main(String[] args) {
+    }
+    /**
+     *
+     * @param radio corresponde ao valor do radio.
+     * @return corresponde á resolución da operación.
+     */
+    public float calcular(double radio){
+        return (float) (radio*2*3.14);
+    }
+
+}
